50c510d8e35cb9cbeabe3a34b54aaaa0f974bd3c
F Claude <52762083+whoiscoos@users.noreply.github.com>
2022-02-07 17:40:09 -0700

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include <vector>
+
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
+
+TEST_CASE( "Testing sum function...", "[sum]" ) {
+    std::vector<int> test1{ 0, 2, 4, 6 };
+    REQUIRE( Sum( test1 ) == 12 );
+    std::vector<int> test2{ 1, 3, 5 };
+    REQUIRE( Sum( test1 ) == 9 );
+}
