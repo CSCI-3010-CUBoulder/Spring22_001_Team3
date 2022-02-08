e31df272d0d67ad326ef97489426685890f5d407
F Claude <52762083+whoiscoos@users.noreply.github.com>
2022-02-07 17:50:33 -0700

    fix test

--- test.cpp
+++ test.cpp
@@ -7,6 +7,7 @@
 TEST_CASE( "Testing sum function...", "[sum]" ) {
     std::vector<int> test1{ 0, 2, 4, 6 };
     REQUIRE( Sum( test1 ) == 12 );
+
     std::vector<int> test2{ 1, 3, 5 };
-    REQUIRE( Sum( test1 ) == 9 );
+    REQUIRE( Sum( test2 ) == 9 );
 }
